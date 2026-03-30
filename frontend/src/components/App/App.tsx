6cb2fe393ba9869a357b7e05fa6f429950b12268
Bucher Tim (bucheti1) <bucheti1@students.zhaw.ch>
2026-03-30 19:52:09 +0200

    feature(frontend): add responsive design

--- frontend/src/components/App/App.tsx
+++ frontend/src/components/App/App.tsx
@@ -3,17 +3,20 @@ import {BrowserRouter, Route, Routes, Navigate} from 'react-router';
 import {RootLayout} from '../RootLayout/RootLayout';
 
 import {ROUTES} from '../../routes';
+import {ThemeProvider} from 'next-themes';
 
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path={ROUTES.root} element={<RootLayout />}>
-          <Route index element={<Navigate to={ROUTES.dashboard} replace />} />
-          <Route path={ROUTES.dashboard} element={<ActionDashboard />} />
-        </Route>
-      </Routes>
-    </BrowserRouter>
+    <ThemeProvider attribute="class" defaultTheme="light" enableSystem>
+      <BrowserRouter>
+        <Routes>
+          <Route path={ROUTES.root} element={<RootLayout />}>
+            <Route index element={<Navigate to={ROUTES.dashboard} replace />} />
+            <Route path={ROUTES.dashboard} element={<ActionDashboard />} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
+    </ThemeProvider>
   );
 }
 
