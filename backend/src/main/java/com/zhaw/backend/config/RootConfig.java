cc0eeb639054653b754a6f0493f92e103e90efff
Lachnit Angelo (lachnang) <lachnang@students.zhaw.ch>
2026-04-15 20:54:05 +0200

    fix(faked login): faked login for now to make page work

--- backend/src/main/java/com/zhaw/backend/config/RootConfig.java
+++ backend/src/main/java/com/zhaw/backend/config/RootConfig.java
@@ -2,14 +2,22 @@ package com.zhaw.backend.config;
 
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.FilterType;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.context.annotation.Import;
 
 /**
  * Root ApplicationContext (Parent Context) for Services/Security/Filter etc.
  */
 @Configuration
-@ComponentScan(basePackages = {
-        "com.zhaw.backend.security" // SessionService, AuthService, AuthCookieFilter, SecurityConfig
-})
+@Import(PersistenceConfig.class)
+@ComponentScan(
+        basePackages = "com.zhaw.backend",
+        excludeFilters = {
+                @ComponentScan.Filter(type = FilterType.ANNOTATION, classes = {Controller.class, RestController.class}),
+                @ComponentScan.Filter(type = FilterType.REGEX, pattern = "com\\.zhaw\\.backend\\.config\\..*")
+        }
+)
 public class RootConfig {
 }
-
