e29b8b3a301057cd395dd6c4f3b33227adad6007
Myriam Schaad <schaamyr@students.zhaw.ch>
2026-05-09 18:19:38 +0200

    fix: running prettify again

--- frontend/src/store/slices/ActionSlice.ts
+++ frontend/src/store/slices/ActionSlice.ts
@@ -139,13 +139,9 @@ export const startAction = createAsyncThunk(
   'action/startAction',
   async ({userId, actionId}: {userId: number; actionId: number}, {rejectWithValue}) => {
     try {
-      const response = await apiClient.post(
-        '/actions/startAction',
-        null,
-        {
-          params: {userId, actionId},
-        },
-      );
+      const response = await apiClient.post('/actions/startAction', null, {
+        params: {userId, actionId},
+      });
       return response.data;
     } catch (error: unknown) {
       if (error instanceof Error) {
